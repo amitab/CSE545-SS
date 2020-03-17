62c0d69932e94707fbb244a1ae407fefa843f402
Amitabh Das <amitab.das@outlook.com>
2020-03-17 00:52:51 -0700

    test

--- src/main/java/bankApp/application/Application.java
+++ src/main/java/bankApp/application/Application.java
@@ -12,7 +12,7 @@ import model.User;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Bitch.class, args);
 	}
 
 }
